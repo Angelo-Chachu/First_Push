4ab7314054760614b1b238a78bc048ee60e34d57
Angelo-Chachu <hlayani13@gmail.com>
2021-12-03 14:49:04 +0200

    console add switch statement

---
+++ add.js
@@ -0,0 +1,23 @@
+var num = 8;
+
+switch (num) 
+{
+    case 7:
+        {
+            console.log("value is 7");
+            break;
+        }
+        case 8:
+            {
+                console.log("value is 8")
+                break;
+            }
+            case 9:
+                {
+                    console.log("the valu is 9");
+                    break;
+                }
+    default:
+        console.log("Out of range");
+        break;
+}
